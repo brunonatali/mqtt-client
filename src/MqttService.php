1739e55d9d93188d493ffa1c53459a3b9ba33c5d
Bruno Natali <b010010010n@gmail.com>
2020-07-03 14:59:56 -0300

    New method (different process). separated HID

--- src/MqttService.php
+++ src/MqttService.php
@@ -8,7 +8,10 @@ use React\Socket\TcpConnector;
 
 use BrunoNatali\Tools\Queue;
 use BrunoNatali\Tools\OutSystem;
+use BrunoNatali\Tools\File\JsonFile;
 use BrunoNatali\Tools\Communication\SimpleUnixServer;
+use BrunoNatali\Tools\Communication\SimpleUnixClient;
+use BrunoNatali\WebInterface\HIDInterface;
 
 use BinSoul\Net\Mqtt\Message;
 use BinSoul\Net\Mqtt\Connection;
@@ -21,13 +24,17 @@ use BinSoul\Net\Mqtt\Client\React\ReactMqttClient;
 class MqttService implements MqttServiceInterface
 {
     private $loop;
+    private $queue;
     private $client;
+    private $hidClient;
     private $server;
-    private $hid;
 
+    private $config;
+    private $sysConfig;
     private $dns;
 
     private $id;
+    private $queueTO;
     private $reconnectionScheduled;
 
     Protected $outSystem;
@@ -35,39 +42,76 @@ class MqttService implements MqttServiceInterface
     function __construct()
     {
         $this->loop = Factory::create();
-        $this->client = null;
 
+        // Get serial
         $id = @\file_get_contents('/etc/desh/app/serial');
         if ($id === false)
             $this->id = self::MQTT_CLIENT_ID;
         else
             $this->id = \trim($id);
 
-        $config = [
-            'outSystemName' => 'MqttSrv',
+        $this->loadConfig();
+
+        $this->queueTO = null;
+
+        $this->sysConfig = [
+            'outSystemName' => 'MQTT',
             "outSystemEnabled" => true
         ];
 
-        $this->server = new SimpleUnixServer($this->loop, self::MQTT_SERVICE_SOCKET, [$config]);
+        $this->server = new SimpleUnixServer($this->loop, self::MQTT_SERVICE_SOCKET, $this->sysConfig);
 
-        $this->outSystem = new OutSystem($config);
+        // Connect to HID 
+        $this->hidClient = new SimpleUnixClient($this->loop, 'hid.sock', $this->sysConfig);
 
-        $this->hid = new HID($this->loop, $config);
-        
-        $this->dns = self::MQTT_DEFAULT_DNS;
+        $this->client = null; // Set MQTT client
+
+        $this->queue = new Queue($this->loop);
+
+        $this->outSystem = new OutSystem($this->sysConfig);
 
         $this->reconnectionScheduled = false;
     }
 
+    private function loadConfig()
+    {
+        $rC = JsonFile::readAsArray('/etc/desh/config.json');
+        if (isset($rC['mqtt_broker']))
+            $this->config = $rC['mqtt_broker'];
+        else
+            $this->config = [
+                'enabled' => false,
+                'config' => []
+            ];
+        
+        if (isset($rC['network']) && isset($rC['network']['dns']))
+            $this->dns = $rC['network']['dns'];
+        else
+            $this->dns = self::MQTT_DEFAULT_DNS;
+    }
+
+    private function saveConfig($conf): bool
+    {
+        $this->loadConfig();
+
+        $config = [
+            'mqtt_broker' => \array_merge($this->config, $conf)
+        ];
+
+        if (JsonFile::saveArray( '/etc/desh/config.json', $config, 
+            false, JSON_PRETTY_PRINT))
+            return true;
+
+        return false;
+    }
+
     public function start()
     {
+        $this->configureCallbacks();
+
         $this->connectToBroker();
 
-        // $this->configureCallbacks();
-
-        $this->hid->start(function ($sensor, $value) {
-            $this->postSensor($sensor, $value);
-        });
+        $this->hidClient->connect();
 
         $this->loop->run();
     }
@@ -109,22 +153,64 @@ class MqttService implements MqttServiceInterface
         return false;
     }
 
-    public function postSensor($sensor, $value)
+    public function postSensor($sensor, $value, $ts)
     {
-        $topic = self::MQTT_TENANT  . '/sensors/' . $this->id;
+        $topic = $this->config['config']['tenant']  . '/sensors/' . $this->id;
         $value = [
             'sensor' => $sensor,
             'value' => $value,
-            'ts' => \time()
+            'ts' => $ts
         ];
 
         return $this->publish($topic, \json_encode($value));
+    }
+
+    private function postConfig($type, $name, $value, $ts)
+    {
+        //a/config/+/1234567890abcdef12
+
+        if ($type === 'sensor') {
+            $topic = $this->config['config']['tenant']  . '/config/sensors/' . $this->id;
+            $value = [
+                'user' => 'device',
+                [
+                    'sensor' => $name,
+                    'period' => $value,
+                    'ts' => $ts
+                ]
+            ];
+        } else if ($type === 'device') {
+            $topic = $this->config['config']['tenant']  . '/config/device/' . $this->id;
+            $value = [
+                'user' => 'device',
+                [
+                    $name => $value,
+                    'ts' => $ts
+                ]
+            ];
+        }
+
+        return $this->publish($topic, \json_encode($value));
+    }
+
+    private function configureSensor($name, $time)
+    {
+        $this->queue->push(function () use ($name, $time) {
+            $this->hidClient->write(\json_encode([
+                'ident' => HIDInterface::HID_DATA_TYPE_CFG,
+                'name' => $name,
+                'time' => $time
+            ]));
+        });
     }
 
     public function configureByJson($config)
     {
         if (!isset($config['user']))
             return "No user defined";
+
+        if ($config['user'] === 'device')
+            return "Configuration from device";
 
         $user = $config['user'];
         unset($config['user']);
@@ -157,7 +243,12 @@ class MqttService implements MqttServiceInterface
 
                 }
             } else if ($key === 'broker') {
-                $this->connectToBroker($conf);
+                if ($this->saveConfig($conf)) {
+                    $this->loadConfig();
+                    $this->connectToBroker();
+                } else {
+                    return "Broker config not accepted";
+                }
             } else {// Sensor
                 if (!isset($conf['sensor']))
                     return "Misconfigured sensor";
@@ -168,6 +259,7 @@ class MqttService implements MqttServiceInterface
                 if (!isset($conf['ts']))
                     return "Misconfigured ts";    
 
+                /* Not handled in this version
                 if ($conf['sensor'] === 'global') {
                     foreach ($this->hid->getInterfaceByName(true) as $interface) {
                         $interface['time'] = $conf['period'];
@@ -181,18 +273,11 @@ class MqttService implements MqttServiceInterface
 
                     break;
                 }
-                    
-                if (($interface = $this->hid->getInterfaceByName($conf['sensor'])) === false)
-                    return "Mismatch sensor";
-
-                $interface['time'] = $conf['period'];
-                $this->hid->registerInterface(
-                    $interface, 
-                    function ($sensor, $value) {
-                        $this->postSensor($sensor, $value);
-                    }
-                );
+                */
+                $this->configureSensor($conf['sensor'], $conf['period']);
             }
+
+            
         }
 
         return true;
@@ -208,10 +293,14 @@ class MqttService implements MqttServiceInterface
         return true;
     }
 
-    private function createClient($config)
+    private function createClient()
     {
         $dnsResolverFactory = new \React\Dns\Resolver\Factory();
-        $connector = new DnsConnector(new TcpConnector($this->loop), $dnsResolverFactory->createCached($this->dns, $this->loop));
+
+        $connector = new DnsConnector(
+            new TcpConnector($this->loop), 
+            $dnsResolverFactory->createCached($this->dns, $this->loop)
+        );
         $this->client = new ReactMqttClient($connector, $this->loop);
         
 
@@ -226,13 +315,22 @@ class MqttService implements MqttServiceInterface
         });
         
         $this->client->on('connect', function (Connection $connection) {
-            // Broker connected
             $this->outSystem->stdout("Broker connected: " . $connection->getClientID(), OutSystem::LEVEL_NOTICE);
         });
         
         $this->client->on('disconnect', function (Connection $connection) {
-            // Broker disconnected
             $this->outSystem->stdout("Broker disconnected: " . $connection->getClientID(), OutSystem::LEVEL_NOTICE);
+
+            $this->scheduleReconnection();
+        });
+        
+        $this->client->on('warning', function (\Exception $e) {
+            $this->outSystem->stdout("Warning: " . $e->getMessage(), OutSystem::LEVEL_NOTICE);
+        });
+        
+        $this->client->on('error', function (\Exception $e) {
+            $this->outSystem->stdout("Broker error: " . $e->getMessage() .
+                ' Scheduling error handler to 10s ...' , OutSystem::LEVEL_NOTICE);
 
             $this->scheduleReconnection();
         });
@@ -249,7 +347,8 @@ class MqttService implements MqttServiceInterface
         
             $data = $message->getPayload();
             $this->outSystem->stdout(
-                ': ' . $message->getTopic().' => ' . mb_strimwidth($data, 0, 50, '...'), 
+                //': ' . $message->getTopic().' => ' . mb_strimwidth($data, 0, 50, '...'), // Truncate $data 50 chars
+                ': ' . $message->getTopic() . " => $data", 
                 OutSystem::LEVEL_NOTICE
             );
 
@@ -257,22 +356,11 @@ class MqttService implements MqttServiceInterface
                 if (!is_bool($result = $this->configureByJson($data)))
                     $this->outSystem->stdout( "Error config: $result" , OutSystem::LEVEL_NOTICE);
                 else 
-                    $this->outSystem->stdout( "Configured." , OutSystem::LEVEL_NOTICE);
+                    $this->outSystem->stdout( "Configured / Scheduled." , OutSystem::LEVEL_NOTICE);
 
             } else {
                 $this->outSystem->stdout( "Wrong data" , OutSystem::LEVEL_NOTICE);
             }
-        });
-        
-        $this->client->on('warning', function (\Exception $e) {
-            $this->outSystem->stdout("Warning: " . $e->getMessage(), OutSystem::LEVEL_NOTICE);
-        });
-        
-        $this->client->on('error', function (\Exception $e) use ($config) {
-            $this->outSystem->stdout("Broker error: " . $e->getMessage() .
-                ' Scheduling error handler to 10s ...' , OutSystem::LEVEL_NOTICE);
-
-            $this->scheduleReconnection();
         });
     }
 
@@ -291,8 +379,10 @@ class MqttService implements MqttServiceInterface
         });
     }
 
-    private function connectToBroker($config = [])
+    private function connectToBroker()
     {
+        $config = &$this->config['config'];
+
         if (!isset($config['uri']))
             $config['uri'] = self::MQTT_BROKER_URI;
             
@@ -325,7 +415,7 @@ class MqttService implements MqttServiceInterface
             }
 
         } else {
-            $this->createClient($config);
+            $this->createClient();
         }
 
         $this->outSystem->stdout(
@@ -336,7 +426,7 @@ class MqttService implements MqttServiceInterface
             
         $this->client->connect(
             $config['uri'],
-            ($config['port'] !== null ? $config['port'] : 1883),
+            (isset($config['port']) && $config['port'] ? $config['port'] : 1883),
             new DefaultConnection(
                 ($config['user'] !== null && $config['password'] !== null ? $config['user'] : ''), 
                 ($config['user'] !== null && $config['password'] !== null ? $config['password'] : ''), 
@@ -344,26 +434,72 @@ class MqttService implements MqttServiceInterface
                 $this->id
             )
         )
-        ->then( function () {
+        ->then( function() use ($config) {
 
             // Subscribe to all configs
-            $this->subscribe(self::MQTT_TENANT . '/config/+/' . $this->id);
+            $this->subscribe($config['tenant']. '/config/+/' . $this->id);
                 
-        }/*, function ($e) use ($config) { // Check if necessary -> Error already handled in createClient()
-            $this->outSystem->stdout("Broker connect error: " . $e->getMessage() .
-                ' Rescheduling to 10s ...' , OutSystem::LEVEL_NOTICE);
-
-            $this->loop->addTimer(10, function () use ($config) {
-                $this->connectToBroker($config);
-            });
-        }*/);
+        });
     }
 
     private function configureCallbacks()
     {
+        $this->hidClient->onData(function ($data){
 
-        $this->server->onData(function ($data, $id) {
+            $this->outSystem->stdout("HIDClient Data: '$data' => ", false, OutSystem::LEVEL_NOTICE);
 
+            $data = \json_decode($data, true);
+
+            if (\is_array($data)) {
+
+                if (isset($data['ident'])) {
+                    if ($data['ident'] === HIDInterface::HID_DATA_TYPE_ACK) { // Config accept
+                        $this->outSystem->stdout("ACK", OutSystem::LEVEL_NOTICE);
+                        $this->dequeue();
+                    } else if ($data['ident'] === HIDInterface::HID_DATA_TYPE_NACK) { // Config not accept
+                        $this->outSystem->stdout("NACK", OutSystem::LEVEL_NOTICE);
+                        $this->dequeue();
+                    } else if ($data['ident'] === HIDInterface::HID_DATA_TYPE_ACQ) { // Sensor data
+                        $this->postSensor($data['name'], $data['value'], $data['ts']);
+                    } else if ($data['ident'] === HIDInterface::HID_DATA_TYPE_CFG) { // Sensor configured
+                        $this->postConfig('sensor', $data['name'], $data['time'], \time());;
+                    } else {
+                        // unrecognized data
+                        
+                        $this->outSystem->stdout("UNKNW", OutSystem::LEVEL_NOTICE);
+                    }
+                }
+
+            } else {
+                $this->outSystem->stdout("ERROR", OutSystem::LEVEL_NOTICE);
+            }
         });
+
+        $this->hidClient->onConnect(function () {
+            $this->outSystem->stdout("Connected to HID", OutSystem::LEVEL_NOTICE);
+            $this->dequeue();
+        });
+
+        $this->hidClient->onClose(function () {
+            //echo "CLOSED !!! " .PHP_EOL;
+        });
+    }
+
+    private function dequeue()
+    {
+        
+        if ($this->queueTO !== null) {
+            $loop->cancelTimer($this->queueTO);
+            $this->queueTO = null;
+        }
+        
+        $this->queue->resume(false); // Just resume
+        if ($run = $this->queue->next(1)) {
+            $this->queueTO = $loop->addTimer(5.0, function () use ($disconnect, &$error) { // Wait 5s before force next
+                $this->dequeue();
+            });
+
+            ($run);
+        }
     }
 }
